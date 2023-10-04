076dda87c30b7a9979ef98ddac889d2f94f6f7be
pinta1008 <pintaricf@gmail.com>
2023-10-04 15:57:29 +0200

    First Commit

---
+++ database/migrations/2023_10_04_135253_create_meals_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('meals', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table ->string('description');
+            $table ->string('status');
+            $table ->integer('category_id')->nullable();
+            $table ->integer('tag_id');
+            $table ->integer('ingredient_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('meals');
+    }
+};
